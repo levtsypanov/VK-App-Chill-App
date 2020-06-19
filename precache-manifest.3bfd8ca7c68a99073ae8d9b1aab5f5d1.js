a27094c88829da3490a265956547bf9cab7dfbcf
levtsypanov <levdark7@gmail.com>
2020-06-19 22:51:55 +0300

    first commit

---
+++ precache-manifest.3bfd8ca7c68a99073ae8d9b1aab5f5d1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df84cad66df3d31fffbdbb3fb57eca84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbb26070efe99562397",
+    "url": "/static/css/2.814df5ce.chunk.css"
+  },
+  {
+    "revision": "f18dcd0372574048ef7d",
+    "url": "/static/css/main.cef8dcd3.chunk.css"
+  },
+  {
+    "revision": "cdbb26070efe99562397",
+    "url": "/static/js/2.c9e18d2c.chunk.js"
+  },
+  {
+    "revision": "f18dcd0372574048ef7d",
+    "url": "/static/js/main.571b1471.chunk.js"
+  },
+  {
+    "revision": "8675d6925f243b9a56c6",
+    "url": "/static/js/runtime-main.eb40d168.js"
+  },
+  {
+    "revision": "a43afe4bcfb64d434c7e5847d18ff27b",
+    "url": "/static/media/beach.a43afe4b.png"
+  },
+  {
+    "revision": "fc2a929732feed693fcdff88d31a2d5e",
+    "url": "/static/media/bird.fc2a9297.png"
+  },
+  {
+    "revision": "9930d3d34b65983fae47c3c88f0e55c9",
+    "url": "/static/media/catpurr.9930d3d3.png"
+  },
+  {
+    "revision": "a6b3e8edd0a04bdcf3df70e3a8c99ef6",
+    "url": "/static/media/drop.a6b3e8ed.png"
+  },
+  {
+    "revision": "049918eb38c28ba322a4dab93249f869",
+    "url": "/static/media/fire.049918eb.png"
+  },
+  {
+    "revision": "45780598fce955f0df734959989cfbee",
+    "url": "/static/media/forest.45780598.png"
+  },
+  {
+    "revision": "b6d17c48a624f7054ebc780bfc7ed869",
+    "url": "/static/media/leaves.b6d17c48.png"
+  },
+  {
+    "revision": "1e3b5c5b19c56fc75566c7acfcde2c0f",
+    "url": "/static/media/rain.1e3b5c5b.png"
+  },
+  {
+    "revision": "b35b29cf4a14b94506d220c706218bc9",
+    "url": "/static/media/river.b35b29cf.png"
+  },
+  {
+    "revision": "8d2769b4a4df18c170e0293b32820e1c",
+    "url": "/static/media/sheep.8d2769b4.png"
+  },
+  {
+    "revision": "4ab42539e87f58aa02c467094afd4ef9",
+    "url": "/static/media/thunder.4ab42539.png"
+  }
+]);
